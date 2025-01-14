4ffd7bbcaa239dfd2523a00a4ea0190b47b5d512
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2025-01-14 23:44:36 +0200

    build the courses app urls

---
+++ educa/courses/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from . import views
+
+app_name = 'courses'
+
+urlpatterns = [
+    path(
+        'mine/',
+        views.ManageCourseListView.as_view(),
+        name='manage-course-list'
+    ),
+    path(
+        'create/',
+        views.CourseCreateView.as_view(),
+        name='course-create'
+    ),
+    path(
+        '<pk>/edit/',
+        views.CourseUpdateView.as_view(),
+        name='course-edit'
+    ),
+    path(
+        '<pk>/delete/',
+        views.CourseDeleteView.as_view(),
+        name='course-delete'
+    ),
+]
