6b99be7d9f9f75f5daa8207d4de233792da4ab1f
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2025-01-15 00:02:58 +0200

    resolving some issues

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-    path('course/', include('courses.urls', namespace='courses'))
+    path('course/', include('courses.urls'))
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
