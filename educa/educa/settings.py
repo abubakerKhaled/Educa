8303cc5cbe0f2839a69c343cfea5ae1c78fa45cd
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2025-02-04 00:05:56 +0200

    fix: login and logout redirect link

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -124,6 +124,9 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+LOGIN_REDIRECT_URL = '/'  # Redirects to the dashboard after login
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
